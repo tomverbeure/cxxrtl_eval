daa07a4068ad8233f9ad42d7346c17c8c5c06de6
Tom Verbeure <hombre+github@gmail.com>
2020-06-12 16:23:48 -0700

    Verilator makefile

--- verilator/build.sh
+++
@@ -1,8 +0,0 @@
-
-rm -fr obj_dir
-verilator -CFLAGS "-g -O3" -Wno-fatal -cc tb.v ../spinal/ExampleTop.sim.v 
-cd obj_dir
-make -f Vtb.mk
-cd ..
-g++ -std=c++11 -g -O3 -I/opt/verilator/share/verilator/include/ -I/opt/verilator/share/verilator/include/vltstd -Iobj_dir main.cpp /opt/verilator/share/verilator/include/verilated.cpp ./obj_dir/Vtb__ALL.a -o tb
-cp ../spinal/*.bin .
--- verilator/main.cpp
+++ verilator/main.cpp
@@ -1,19 +1,68 @@
 #include <stdlib.h>
 #include "Vtb.h"
+
+#ifdef TRACE_VCD
+#include "verilated_vcd_c.h"
+#endif
+
+#ifdef TRACE_FST
+#include "verilated_fst_c.h"
+#endif
+
 #include "verilated.h"
 
 int main(int argc, char **argv) 
 {
     Verilated::commandArgs(argc, argv);
 
-    Vtb *tb = new Vtb;
+    int nr_cycles = 1000000;
+    if (argc ==2)
+        nr_cycles = atoi(argv[1]);
 
-    for(int i=0;i<1000000;++i){
+    Vtb *tb = new Vtb;
+#if defined(TRACE_VCD)
+    VerilatedVcdC *trace;
+#endif
+#if defined(TRACE_FST)
+    VerilatedFstC *trace;
+#endif
+
+#if defined(TRACE_VCD) || defined(TRACE_FST)
+    Verilated::traceEverOn(true);
+#endif
+
+#if defined(TRACE_VCD)
+    trace = new VerilatedVcdC;
+    tb->trace(trace, 99);
+    trace->open("waves.vcd");
+#endif
+
+#if defined(TRACE_FST)
+    trace = new VerilatedFstC;
+    tb->trace(trace, 99);
+    trace->open("waves.fst");
+#endif
+
+    for(int i=0;i<nr_cycles;++i){
         tb->osc_clk = 1;
         tb->eval();
+#if defined(TRACE_VCD) || defined(TRACE_FST)
+        trace->dump(i*2);
+#endif
+
         tb->osc_clk = 0;
         tb->eval();
+
+#if defined(TRACE_VCD) || defined(TRACE_FST)
+        trace->dump(i*2+1);
+        trace->flush();
+#endif
+
     } 
+
+#if defined(TRACE_VCD) || defined(TRACE_FST)
+    trace->close();
+#endif
     
     exit(EXIT_SUCCESS);
 }
